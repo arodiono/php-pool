9a1f9e261186ec0723489ec61960a335312f7140
Andrii Rodionov <arodiono@student.unit.ua>
2017-06-27 11:40:28 +0300

    Update config.php

--- rush01/config.php
+++ rush01/config.php
@@ -1,6 +1,6 @@
 <?php
 define('SQL_HOST', 'localhost');
-define('SQL_USER', 'root');
-define('SQL_PASS', 'XmmD3RKE');
-define('SQL_DB', 'warhummer');
+define('SQL_USER', 'db_usermane');
+define('SQL_PASS', 'db_password');
+define('SQL_DB', 'db_name');
 ?>
